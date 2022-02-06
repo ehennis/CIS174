bcac9900e2167e0d2e928634b1ab83aa16a9a7f7
Evan Hennis <evanhennis@gmail.com>
2022-02-05 22:27:52 -0600

    Content for Week 5

---
+++ 2022-SPR/Week05 Demo/RoutingPractice/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoutingPractice.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Home");
+        }
+
+        public IActionResult Privacy()
+        {
+            return Content("Privacy");
+        }
+
+        public IActionResult Display(string id)
+        {
+            if (id == null)
+            {
+                return Content("No ID supplied.");
+            }
+            else
+            {
+                return Content("ID: " + id);
+            }
+        }
+                
+        //public IActionResult StaticContent(string num)
+        //{
+        //    return Content($"Static Content: {num}");
+        //}
+        //
+        //[Route("/")]
+        //public IActionResult IndexOverride()
+        //{
+        //    return Content("Index Override!");
+        //}
+
+
+    }
+}
