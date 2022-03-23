ffb1d6d0b64a44905f09cc5bf9115a95f9681a31
Evan Hennis <evanhennis@gmail.com>
2022-03-22 22:40:51 -0500

    Week 10 Stuff

---
+++ 2022-SPR/Week10 Demo/Week10/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Week10.Models
+{
+    public class Movie
+    {
+        [MovieFirstName]
+        [Required(ErrorMessage = "Please enter a name")]
+        [StringLength(30)]
+        public string Name { get; set; }
+
+        [Range(1,5, ErrorMessage = "Please enter a rating between 1 and 5")]
+        public int Rating { get; set; }
+    }
+}
---
+++ 2022-SPR/Week10 Demo/Week10/wwwroot/js/moviename.js
@@ -0,0 +1,12 @@
+﻿$(function () {
+
+    jQuery.validator.addMethod("moviefirstname",
+        function (value, element, param) {
+            if (value == '') return false;
+            if (value == 'EvanH') return false;
+            return true;
+        }, jQuery.validator.format('Client Side Validation'));
+
+    jQuery.validator.unobtrusive.adapters.addSingleVale("moviefirstname","name");
+
+}(jQuery));
