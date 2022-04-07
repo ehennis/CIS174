a7245b88c19e66bc8f0b13b48d7944b268b95615
Evan Hennis <evanhennis@gmail.com>
2022-04-06 19:26:45 -0500

    Created new test classes

---
+++ 2022-SPR/Week12Demo/Week12Test/BookRepositoryTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Week11.Models;
+using Week12.Repository;
+using Xunit;
+using Moq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.InMemory;
+
+namespace Week12Test
+{
+    public class BookRepositoryTest
+    {
+        private BookRepository repo;
+
+        public BookRepositoryTest()
+        {
+            DbContextOptions<BookstoreContext> options = new DbContextOptionsBuilder<BookstoreContext>()
+                .UseInMemoryDatabase("DbName").Options;
+
+            BookstoreContext context = new BookstoreContext(options);
+            context.Books.Add(new Book() { BookId = 1, Title = "Book One", ISBN = "10" });
+            context.Books.Add(new Book() { BookId = 2, Title = "Book Two", ISBN = "11"   });
+            context.Books.Add(new Book() { BookId = 3, Title = "Book Three", ISBN = "12" });
+            context.SaveChanges();
+
+            repo = new BookRepository(context);
+
+        }
+
+        [Fact]
+        public void GetBook_HappyPath()
+        {
+            var bk = repo.GetBook();
+            Assert.NotNull(bk);
+            Assert.Equal(1, bk.BookId);
+        }
+    }
+}
---
+++ 2022-SPR/Week12Demo/Week12Test/BookServiceTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+using Week11.Models;
+using Week12.Service;
+using Week12.Repository;
+
+namespace Week12Test
+{
+    public class BookServiceTest
+    {
+        private BookService service;
+
+        public BookServiceTest()
+        {
+            Mock<IBookRepository> repo = new Mock<IBookRepository>();
+            repo.Setup(r => r.GetBook()).Returns(new Book());
+
+
+            service = new BookService(repo.Object);
+        }
+
+        [Fact]
+        public void GetBook_HappyPath()
+        {
+            var book = service.GetBook();
+            Book bk = Assert.IsType<Book>(book);
+        }
+    }
+}
