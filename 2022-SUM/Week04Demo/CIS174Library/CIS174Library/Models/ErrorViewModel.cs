97e5823d13811a7a18772dc5be4651be5a95909b
Evan Hennis <evanhennis@gmail.com>
2022-06-11 10:23:12 -0500

    Moved demo to week 5

--- 2022-SUM/Week04Demo/CIS174Library/CIS174Library/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CIS174Library.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
