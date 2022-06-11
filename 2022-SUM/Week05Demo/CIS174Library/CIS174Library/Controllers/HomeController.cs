7fe485c7d176f05a640c69025300f6452b0a0388
Evan Hennis <evanhennis@gmail.com>
2022-06-11 10:46:29 -0500

    Added week 5 demo code

--- 2022-SUM/Week05Demo/CIS174Library/CIS174Library/Controllers/HomeController.cs
+++ 2022-SUM/Week05Demo/CIS174Library/CIS174Library/Controllers/HomeController.cs
@@ -30,6 +30,8 @@ namespace CIS174Library.Controllers
             return View();
         }
 
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
