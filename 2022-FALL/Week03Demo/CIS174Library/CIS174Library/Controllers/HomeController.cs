5bf629cd351efcce4b9fae9ad19f888a86ec494c
Evan Hennis <evanhennis@gmail.com>
2022-09-05 10:40:47 -0500

    Added week 3 project

---
+++ 2022-FALL/Week03Demo/CIS174Library/CIS174Library/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using CIS174Library.Data;
+using CIS174Library.Models;
+using CIS174Library.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CIS174Library.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private ILibraryRepository libraryRepository;
+
+        public HomeController(ILogger<HomeController> logger, ILibraryRepository repo)
+        {
+            _logger = logger;
+            libraryRepository = repo;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Student";
+
+            List<Book> books = libraryRepository.GetAllBooks();
+            return View(books);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
