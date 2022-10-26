30fa9e563f17086151e857a22d7426e0fb0fdce0
Evan Hennis <evanhennis@gmail.com>
2022-10-25 20:18:28 -0500

    Removed all code from the instructions.

--- 2022-FALL/Week10Demo/CIS174Library/CIS174Library/Models/Book.cs
+++ 2022-FALL/Week10Demo/CIS174Library/CIS174Library/Models/Book.cs
@@ -7,12 +7,8 @@ namespace CIS174Library.Models
         // EF will instruct the database to automatically generate this value
         public int BookId { get; set; }
 
-        [BookName]
-        [Required(ErrorMessage = "Please enter a name.")]
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
-        [Required(ErrorMessage = "Please enter a year.")]
-        [Range(1889, 2050, ErrorMessage = "Year must be between 1889 and now.")]
         public int? Year { get; set; }
     }
 }
