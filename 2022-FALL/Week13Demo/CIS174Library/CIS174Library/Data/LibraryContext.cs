a0013654b43fc46d16e85a661296833703c83663
Evan Hennis <evanhennis@gmail.com>
2022-11-16 16:09:36 -0600

    Adding week 13 stuff

---
+++ 2022-FALL/Week13Demo/CIS174Library/CIS174Library/Data/LibraryContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CIS174Library.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace CIS174Library.Data
+{
+    public class LibraryContext : IdentityDbContext<User>
+    {
+        public LibraryContext(DbContextOptions<LibraryContext> options) : base(options)
+        { }
+
+        public DbSet<Book> Books { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Book>().HasData(
+                new Book
+                {
+                    BookId = 1,
+                    Name = "Core MVC",
+                    Year = 2022
+                },
+                new Book
+                {
+                    BookId = 2,
+                    Name = "The C++ Programming Language",
+                    Year = 2000
+                });
+        }
+    }
+}
