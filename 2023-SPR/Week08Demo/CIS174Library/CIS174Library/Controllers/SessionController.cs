246d97ebd67f1283a224d26c4128d2432b4c1c49
Evan Hennis <evanhennis@gmail.com>
2023-03-02 18:43:15 -0600

    Updated code to start from scratch

--- 2023-SPR/Week08Demo/CIS174Library/CIS174Library/Controllers/SessionController.cs
+++ 2023-SPR/Week08Demo/CIS174Library/CIS174Library/Controllers/SessionController.cs
@@ -6,21 +6,11 @@ namespace CIS174Library.Controllers
     {
         public IActionResult Index()
         {
-            int num = HttpContext.Session.GetInt32("num") ?? 0;
-            num += 1;
-            HttpContext.Session.SetInt32("num", num);
-
-            var options = new CookieOptions { Expires = DateTime.Now.AddDays(30) };
-            Response.Cookies.Append("username", "evan", options);
-
             return View();
         }
 
         public IActionResult Cookie()
         {
-            var usrname = Request.Cookies["username"];
-            ViewBag.UserName = usrname;
-
             return View();
         }
     }
