12021c5e8eafe0122b2938f7401a3d5c6bb4c9e1
Evan Hennis <evanhennis@gmail.com>
2023-03-23 17:48:05 -0500

    Added week 10 files

---
+++ 2023-SPR/Week10Demo/CIS174Library/CIS174Library/wwwroot/js/bookname.js
@@ -0,0 +1,14 @@
+﻿
+
+$.validator.addMethod("bookname",
+        function (value, element, param) {
+            if (value == '') return false;
+            if (value == 'EvanH') return false;
+            return true;
+        });
+
+$.validator.unobtrusive.adapters.add("bookname", ["name"], function (options) {
+    options.rules["bookname"] = options.params.name;
+    options.messages["bookname"] = "Client Side Validation";
+});
+
