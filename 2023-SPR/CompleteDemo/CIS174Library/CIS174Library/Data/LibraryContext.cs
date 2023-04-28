2e73db165dc04327b463e9d1fdcafb9de8d139ae
Evan Hennis <evanhennis@gmail.com>
2023-04-27 21:00:53 -0500

    Final files

---
+++ 2023-SPR/CompleteDemo/CIS174Library/CIS174Library/Data/LibraryContext.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CIS174Library.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+namespace CIS174Library.Data
+{
+    public class LibraryContext : IdentityDbContext<User>
+    {
+        public LibraryContext(DbContextOptions<LibraryContext> options) : base(options)
+        { }
+
+        public DbSet<Book> Books { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Book>().HasData(
+                new Book
+                {
+                    BookId = 1,
+                    Name = "Core MVC",
+                    Year = 2022
+                },
+                new Book
+                {
+                    BookId = 2,
+                    Name = "The C++ Programming Language",
+                    Year = 2000
+                });
+        }
+
+
+        public static async Task CreateAdminUser(IServiceProvider serviceProvider)
+        {
+            using (var scoped = serviceProvider.CreateScope())
+            {
+                UserManager<User> userManager = scoped.ServiceProvider.GetRequiredService<UserManager<User>>();
+                RoleManager<IdentityRole> roleManager = scoped.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+                string username = "admin";
+                string pwd = "admin";
+                string roleName = "Admin";
+
+                // if role doesn't exist, create it
+                if (await roleManager.FindByNameAsync(roleName) == null)
+                {
+                    await roleManager.CreateAsync(new IdentityRole(roleName));
+                }
+
+                if (await userManager.FindByNameAsync(username) == null)
+                {
+                    User user = new User() { UserName = username };
+                    var result = await userManager.CreateAsync(user, pwd);
+                    if (result.Succeeded)
+                    {
+                        await userManager.AddToRoleAsync(user, roleName);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 2023-SPR/CompleteDemo/CIS174Library/CIS174Library/TagHelpers/ButtonTagHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CIS174Library.TagHelpers
+{
+    public class ButtonTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.SetAttribute("class", "btn btn-primary");
+            output.Attributes.SetAttribute("Evan", "Awesome");
+        }
+    }
+}
