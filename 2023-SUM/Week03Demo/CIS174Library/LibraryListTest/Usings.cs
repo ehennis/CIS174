6e52d76f62dd48ac6438efbf4ea3a21cdece4bfc
Evan Hennis <evanhennis@gmail.com>
2023-06-20 17:38:35 -0500

    changed week to module for summer

--- 2023-SUM/Week03Demo/CIS174Library/LibraryListTest/Usings.cs
+++
@@ -1,5 +0,0 @@
-global using Microsoft.VisualStudio.TestTools.UnitTesting;
-global using CIS174Library.Controllers;
-global using CIS174Library.Models;
-global using CIS174Library.Repository;
-global using Moq;
