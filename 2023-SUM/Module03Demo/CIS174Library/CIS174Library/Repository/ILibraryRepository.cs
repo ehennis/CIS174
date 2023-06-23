8e680e04da222ed2b0bdf6d6aa6966a4c05525bd
Evan Hennis <evanhennis@gmail.com>
2023-06-22 19:17:35 -0500

    Renamed the folders

---
+++ 2023-SUM/Module03Demo/CIS174Library/CIS174Library/Repository/ILibraryRepository.cs
@@ -0,0 +1,14 @@
+﻿using CIS174Library.Models;
+
+namespace CIS174Library.Repository
+{
+    public interface ILibraryRepository
+    {
+        List<Book> GetAllBooks();
+        Book Find(int id);
+        void Save();
+        void InsertBook(Book book);
+        void DeleteBook(Book book);
+        void UpdateBook(Book book);
+    }
+}
---
+++ 2023-SUM/Module03Demo/CIS174Library/LibraryListTest/LibraryControllerTest.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryListTest
+{
+    [TestClass]
+    public class LibraryControllerTest
+    {
+        public LibraryControllerTest() 
+        {
+            
+        }
+
+        // Step 7
+        //
+        //
+        //[TestMethod]
+        //public void LibraryController_Add()
+        //{
+        //    Mock<ILibraryRepository> repo = new Mock<ILibraryRepository>();
+        //
+        //    LibraryController ctrl = new LibraryController(repo.Object);
+        //    var ret = ctrl.Add();
+        //
+        //    Assert.IsNotNull(ret);
+        //    Assert.IsInstanceOfType(ret, typeof(ViewResult));
+        //}
+        //
+        //[TestMethod]
+        //public void LibraryController_Edit_Book10()
+        //{
+        //    Mock<ILibraryRepository> repo = new Mock<ILibraryRepository>();
+        //    repo.Setup(x => x.Find(It.IsAny<int>())).Returns(new Book() {  BookId = 10, Name ="Test Book", Year = 2023});
+        //
+        //    LibraryController ctrl = new LibraryController(repo.Object);
+        //    var ret = ctrl.Edit(1);
+        //
+        //    Assert.IsNotNull(ret);
+        //    Assert.IsInstanceOfType(ret, typeof(ViewResult));
+        //
+        //    var vr = ret as ViewResult;
+        //    var bk = vr.Model as Book;
+        //
+        //    Assert.AreEqual(-1, bk.Year);
+        //}
+        //
+        //[TestMethod]
+        //public void LibraryController_Edit_Book_Normal()
+        //{
+        //    Mock<ILibraryRepository> repo = new Mock<ILibraryRepository>();
+        //    repo.Setup(x => x.Find(It.IsAny<int>())).Returns(new Book() { BookId = 11, Name = "Test Book", Year = 2023 });
+        //
+        //    LibraryController ctrl = new LibraryController(repo.Object);
+        //    var ret = ctrl.Edit(1);
+        //
+        //    Assert.IsNotNull(ret);
+        //    Assert.IsInstanceOfType(ret, typeof(ViewResult));
+        //
+        //    var vr = ret as ViewResult;
+        //    var bk = vr.Model as Book;
+        //
+        //    Assert.AreEqual(2023, bk.Year);
+        //}
+    }
+}
