e97001a1cff4803521d2234bbd83a0b6d293ca5d
Evan Hennis <evanhennis@gmail.com>
2023-10-04 17:29:02 -0500

    Adding week 7 files

---
+++ 2023-FALL/Week07Demo/CIS174Library/CIS174Library/Data/LibraryContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CIS174Library.Models;
+
+namespace CIS174Library.Data
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions<LibraryContext> options) : base(options)
+        { }
+
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>().HasData(
+                new Book
+                {
+                    BookId = 1,
+                    Name = "Core MVC",
+                    Year = 2022
+                },
+                new Book
+                {
+                    BookId = 2,
+                    Name = "The C++ Programming Language",
+                    Year = 2000
+                });
+        }
+    }
+}
