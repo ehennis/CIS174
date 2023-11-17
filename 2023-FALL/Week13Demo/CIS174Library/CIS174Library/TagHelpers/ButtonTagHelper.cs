2ba0e448ad4d799f5227b7546f1cad1db5eb2b8b
Evan Hennis <evanhennis@gmail.com>
2023-11-16 18:01:01 -0600

    Adding week 13 files

---
+++ 2023-FALL/Week13Demo/CIS174Library/CIS174Library/TagHelpers/ButtonTagHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CIS174Library.TagHelpers
+{
+    // Added an 'x' so that all buttons won't be blue
+    public class ButtonxTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.SetAttribute("class", "btn btn-primary");
+            output.Attributes.SetAttribute("Evan", "Awesome");
+        }
+    }
+}
