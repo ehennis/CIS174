ba01db0a2d5c681f767f18ba08ef8cced4df108d
Evan Hennis <evanhennis@gmail.com>
2024-10-10 18:06:34 -0500

    Adding week 8 files

---
+++ 2024-FALL/Week08Demo/CIS174Library/CIS174Library/Controllers/SessionController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CIS174Library.Controllers
+{
+    public class SessionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cookie()
+        {
+            return View();
+        }
+    }
+}
---
+++ 2024-FALL/Week08Demo/CIS174Library/CIS174Library/Program.cs
@@ -0,0 +1,42 @@
+using CIS174Library.Data;
+using CIS174Library.Repository;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// Add DbContext
+builder.Services.AddDbContext<LibraryContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("LibraryContext")));
+
+// Add Libraries
+builder.Services.AddScoped<ILibraryRepository, LibraryRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "Static",
+    pattern: "{controller=Home}/{action}/Page/{num}");
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
