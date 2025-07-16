7e604d8e0a6f53aabbecd51e82fc88dd6814163a
Evan Hennis <evanhennis@gmail.com>
2025-07-15 22:20:50 -0500

    Week 9 files

---
+++ 2025-SUM/CompleteDemo/CIS174Library/CIS174Library/Models/BookName.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace CIS174Library.Models
+{
+    public class BookName : ValidationAttribute, IClientModelValidator
+    {
+        public BookName()
+        {
+        }
+
+        public void AddValidation(ClientModelValidationContext context)
+        {
+            if (!context.Attributes.ContainsKey("data-val"))
+                context.Attributes.Add("data-val", "true");
+            context.Attributes.Add("data-val-bookname-name", "EvanH");
+            context.Attributes.Add("data-val-bookname", GetErrorMessage());
+        }
+
+        public string GetErrorMessage()
+        {
+            return "Custom Validation: Do not use your first name";
+        }
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            return value != null && value.ToString() != "EvanHennis" ? ValidationResult.Success : new ValidationResult(GetErrorMessage());
+        }
+    }
+}
