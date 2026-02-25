2395ec7db58a58b0b6b8d591c1cb45dad23f750d
Evan Hennis <evanhennis@gmail.com>
2026-02-24 19:51:03 -0600

    Adding week 7 files

---
+++ 2026-SPR/Week07Demo/CIS174Library/CIS174Library/Controllers/LibraryController.cs
@@ -0,0 +1,49 @@
+﻿using CIS174Library.Models;
+using CIS174Library.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CIS174Library.Controllers
+{
+    public class LibraryController : Controller
+    {
+        private ILibraryRepository libraryRepository;
+
+        //public MovieController(MovieContext ctx)
+        public LibraryController(ILibraryRepository repository)
+        {
+            //context = ctx
+            this.libraryRepository = repository;
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            ViewBag.Action = "Add";
+            return View("Edit", new Book());
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            ViewBag.Action = "Edit";
+            var movie = this.libraryRepository.Find(id);
+            return View(movie);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var movie = this.libraryRepository.Find(id);
+            return View(movie);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Book book)
+        {
+            //context.Movies.Remove(movie);
+            //context.SaveChanges();
+            this.libraryRepository.DeleteBook(book);
+            return RedirectToAction("Index", "Home");
+        }        
+    }
+}
