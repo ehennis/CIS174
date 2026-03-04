056836db001f4ea2f462e76467a6d3a0cf253bb7
Evan Hennis <evanhennis@gmail.com>
2026-03-03 19:19:28 -0600

    Add week 8 files

---
+++ 2026-SPR/Week08Demo/CIS174Library/CIS174Library/Controllers/LibraryController.cs
@@ -0,0 +1,72 @@
+﻿using CIS174Library.Models;
+using CIS174Library.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CIS174Library.Controllers
+{
+    public class LibraryController : Controller
+    {
+        private ILibraryRepository libraryRepository;
+
+        //public MovieController(MovieContext ctx)
+        public LibraryController(ILibraryRepository repository)
+        {
+            //context = ctx
+            this.libraryRepository = repository;
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            ViewBag.Action = "Add";
+            return View("Edit", new Book());
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            ViewBag.Action = "Edit";
+            var movie = this.libraryRepository.Find(id);
+            return View(movie);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                if (book.BookId == 0)
+                {
+                    this.libraryRepository.InsertBook(book);
+                }
+                else
+                {
+                    this.libraryRepository.UpdateBook(book);
+                }
+                this.libraryRepository.Save();
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Action = (book.BookId == 0) ? "Add" : "Edit";
+                return View(book);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var movie = this.libraryRepository.Find(id);
+            return View(movie);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Book book)
+        {
+            //context.Movies.Remove(movie);
+            //context.SaveChanges();
+            this.libraryRepository.DeleteBook(book);
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ 2026-SPR/Week08Demo/CIS174Library/CIS174LibraryTest/SessionControllerTest.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIS174LibraryTest
+{
+    [TestClass]
+    public class SessionControllerTest
+    {
+        /* 
+        public IActionResult Index()
+        {
+            int num = HttpContext.Session.GetInt32("num") ?? 0;
+            num += 1;
+            HttpContext.Session.SetInt32("num", num);
+
+            var options = new CookieOptions { Expires = DateTime.Now.AddDays(30) };
+            Response.Cookies.Append("username", "evan", options);
+
+            return View();
+        }
+        */
+        public delegate void TryGetValueCallback(string name, out byte[] value);
+        [TestMethod]
+        public void Index_Happy()
+        {
+            var session = new Mock<ISession>();
+
+            byte[] outvalue;
+            byte[] mockvalue = BitConverter.GetBytes(90);
+            // little-endian
+            Array.Reverse(mockvalue);
+
+            session.Setup(x => x.TryGetValue(It.IsAny<string>(), out outvalue))
+                .Returns(true)
+                .Callback(new TryGetValueCallback((string name, out byte[] value) => value = mockvalue));
+                //.Callback((string name, out byte[] value) => value = BitConverter.GetBytes(99));
+
+            var mockHttpContext = new Mock<HttpContext>();
+            var mockRequest = new Mock<HttpRequest>();
+            var mockResponse = new Mock<HttpResponse>();
+
+            mockHttpContext.SetupGet(x => x.Session).Returns(session.Object);
+
+            mockHttpContext.SetupGet(x => x.Request).Returns(mockRequest.Object);
+            mockHttpContext.SetupGet(x => x.Response).Returns(mockResponse.Object);
+
+            var controller = new SessionController();
+            controller.ControllerContext = new Microsoft.AspNetCore.Mvc.ControllerContext
+            {
+                HttpContext = mockHttpContext.Object,
+            };
+
+            var result = controller.Index();
+
+
+        }
+    }
+
+}
