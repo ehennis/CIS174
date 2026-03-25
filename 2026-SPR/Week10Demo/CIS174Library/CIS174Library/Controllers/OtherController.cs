b3012b1a794667fa284caeb99fa962481e91c5c3
Evan Hennis <evanhennis@gmail.com>
2026-03-24 19:18:17 -0500

    Adding Week 10 Files.

---
+++ 2026-SPR/Week10Demo/CIS174Library/CIS174Library/Controllers/OtherController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CIS174Library.Controllers
+{
+    [Route("Custom/[controller]/[action]")]
+    public class OtherController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Other Controller!!");
+        }
+    }
+}
---
+++ 2026-SPR/Week10Demo/CIS174Library/CIS174Library/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CIS174Library.Models
+{
+    public class Book
+    {
+        // EF will instruct the database to automatically generate this value
+        public int BookId { get; set; }
+
+        public string? Name { get; set; }
+
+        public int? Year { get; set; }
+    }
+}
