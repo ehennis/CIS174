d933db8d780aab95f6692f35aa571702c419fd02
Evan Hennis <evanhennis@gmail.com>
2026-03-31 17:39:13 -0500

    Added week 11 files.

---
+++ 2026-SPR/Week11Demo/CIS174Library/CIS174Library/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CIS174Library.Models
+{
+    public class Book
+    {
+        // EF will instruct the database to automatically generate this value
+        public int BookId { get; set; }
+
+        [BookName]
+        [Required(ErrorMessage = "Please enter a name.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter a year.")]
+        [Range(1889, 2050, ErrorMessage = "Year must be between 1889 and now.")]
+        public int? Year { get; set; }
+    }
+}
