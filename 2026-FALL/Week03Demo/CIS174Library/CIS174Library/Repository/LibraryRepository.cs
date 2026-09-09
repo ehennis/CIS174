6494babdf9c1c3061166835003bc1ce81befc2ab
Evan Hennis <evanhennis@gmail.com>
2026-09-09 18:41:51 -0500

    Add week 3 files.

---
+++ 2026-FALL/Week03Demo/CIS174Library/CIS174Library/Repository/LibraryRepository.cs
@@ -0,0 +1,44 @@
+﻿using CIS174Library.Data;
+using CIS174Library.Models;
+
+namespace CIS174Library.Repository
+{
+    public class LibraryRepository : ILibraryRepository
+    {
+        private LibraryContext context;
+        public LibraryRepository(LibraryContext cntx)
+        {
+            context = cntx;
+        }
+        public List<Book> GetAllBooks()
+        {
+            return context.Books.ToList();
+        }
+        public Book Find(int id)
+        {
+            return context.Books.Find(id);
+        }
+
+        public void DeleteBook(Book book)
+        {
+            context.Books.Remove(book);
+            context.SaveChanges();
+        }
+
+
+        public void InsertBook(Book book)
+        {
+            context.Books.Add(book);
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        public void UpdateBook(Book book)
+        {
+            context.Books.Update(book);
+        }
+    }
+}
---
+++ 2026-FALL/Week03Demo/CIS174Library/LibraryListTest/Usings.cs
@@ -0,0 +1,5 @@
+global using Microsoft.VisualStudio.TestTools.UnitTesting;
+global using CIS174Library.Controllers;
+global using CIS174Library.Models;
+global using CIS174Library.Repository;
+global using Moq;
